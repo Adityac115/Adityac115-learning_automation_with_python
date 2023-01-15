00ed00bf9bc9dbf8a53e1383c0f97fa797fa6ed0
Adityac115 <adityakushwahaiffco@gmail.com>
2023-01-15 08:45:33 +0530

    finds the file

--- linux_python/findJane.sh
+++ linux_python/findJane.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-
->oldFiles.txt
 files=$(grep " jane " ../python/list.txt | cut -d' ' -f3)
 for f in $files; do 
-  if [ -e $HOME$f ]; then
-    echo $HOME$f >> oldFiles.txt;
+  if [ -e "$HOME$f" ]; then
+    echo "$HOME$f" >> oldFiles.txt;
   fi
 done
